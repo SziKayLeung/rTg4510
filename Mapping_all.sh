48354586d996c96bb8601f40fd26b7bdaf48279c
sl693 <sl693@exeter.ac.uk>
2019-04-12 16:03:43 +0100

    remove output and count

--- Mapping_all.sh
+++ Mapping_all.sh
@@ -7,32 +7,28 @@
 #PBS -m e -M sl693@exeter.ac.uk # email me at job completion
 
 ##############################################################################################################
-# Date: 11th April 2019
-# run Isoseq3 for all Tg4510 samples: L22, K18, O23, S18, K17 
-# already run Isoseq3 as test on S23 sample (10th April S23_Isoseq3.sh)
+# Date: 12th April 2019
+# run GMAP for all Tg4510 samples: L22, K18, O23, S18, K17 
+# already Isoseq3
 #############################################################################################################
 module load GMAP-GSNAP/2016-11-07-foss-2016b
 gmap --version
-
 module load SAMtools/1.7-foss-2018a
 samtools --version
-
-SAMPLES_NAMES=(L22 K18 S18 K17 O23)
+#############################################################################################################
 # directory for saving output
 cd /gpfs/ts0/scratch/sl693/WholeTranscriptome/MAP
-
 # determine path directory for input data
 POLISH=/gpfs/ts0/scratch/sl693/WholeTranscriptome/Isoseq3/POLISH
 
-count=0
+SAMPLES_NAMES=(L22 K18 S18 K17 O23)
+#############################################################################################################
 for map in "${SAMPLES_NAMES[@]}"; do 
   echo "Processing $map file for mapping on GMAP"
-  output=(${SAMPLES_NAMES[count]})
-  gmap -D /gpfs/ts0/home/sl693/reference -d GRCm38.p4 -f samse -n 0 -t 12 -z sense_force $POLISH/"$map.polished.hq.fastq" > $output.isoforms.fastq.sam \
-  2> $output.sam.log
-  echo "Mapping $output succeeded"
-  sort -k 3,3 -k 4,4n $output.hq_isoforms.fastq.sam > $output.hq_isoforms.fastq.sorted.sam
+  gmap -D /gpfs/ts0/home/sl693/reference -d GRCm38.p4 -f samse -n 0 -t 12 -z sense_force $POLISH/"$map.polished.hq.fastq" > $map.isoforms.fastq.sam \
+  2> $map.sam.log
+  echo "Mapping $map succeeded"
+  sort -k 3,3 -k 4,4n $map.hq_isoforms.fastq.sam > $map.hq_isoforms.fastq.sorted.sam
   # different sort from recommended using samtools: https://www.biostars.org/p/93559/
-  samtools sort -O sam -T $output.sorted. -o $output.sort.sam $output.hq_isoforms.fastq.sam
-  count=$((count+1)) 
+  samtools sort -O sam -T $map.sorted. -o $map.sort.sam $map.hq_isoforms.fastq.sam
 done
