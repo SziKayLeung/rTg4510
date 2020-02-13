d79c83a3d917e68db2d802e96812149f8bbb7a23
sl693 <sl693@exeter.ac.uk>
2020-02-13 17:18:24 +0000

    whole_chain.sh: run_fusion_finder on all individual samples and run_sqanti2_QC_fusioninput
    Former-commit-id: 15c086467e0e1d681c95bb5ddbde62204c6d5e62

--- Isoseq3/whole_chain.sh
+++ Isoseq3/whole_chain.sh
@@ -7,12 +7,19 @@
 #PBS -m e -M sl693@exeter.ac.uk # email me at job completion
 
 # 12/02/2020: chain whole transcriptome data, all samples using cupcake data from Isoseq3.2.1 
+# 13/02/2020: run_fusion_finder on all individual samples and run_sqanti2_QC_fusioninput 
 
 ##*************************************** Define variables
 sl693=/gpfs/mrc0/projects/Research_Project-MRC148213/sl693
 CHAIN=$sl693/WholeTranscriptome/Individual/Isoseq3.2.1/CHAIN
-CUPCAKE=$sl693/WholeTranscriptome/Individual/Isoseq3.2.1/TOFU
-CHAIN_SCRIPT=$sl693/softwares/Post_Isoseq3/cDNA_Cupcake/cupcake/tofu/counting
+CHAIN_FUSION=$sl693/WholeTranscriptome/Individual/Isoseq3.2.1/CHAIN_FUSION
+TOFU=$sl693/WholeTranscriptome/Individual/Isoseq3.2.1/TOFU
+TOFU_FUSION=$sl693/WholeTranscriptome/Individual/Isoseq3.2.1/TOFU_FUSION
+POLISHED=$sl693/WholeTranscriptome/Individual/Isoseq3.2.1/Isoseq3_WKD/CLUSTER
+MAPPING=$sl693/WholeTranscriptome/Individual/Isoseq3.2.1/MAPPING
+GENERAL_SCRIPT=$sl693/Scripts/general
+SQANTI2=$sl693/WholeTranscriptome/Individual/Isoseq3.1.2/SQANTI2
+SQANTI2_output_dir=$sl693/WholeTranscriptome/Individual/Isoseq3.2.1/SQANTI2
 
 ###*************************************** Process all whole transcriptome samples using tofu_cupcake chain.samples.py 
 # Prepare files for chaining as according to Liz's chain.py 
@@ -23,7 +30,7 @@ mkdir O18 K18 S18 L22 Q20 K24 Q21 K17 M21 O23 S23 K23
 for i in ${SAMPLE_NAMES[@]}; do
     echo "Processing $i"
     cd $CHAIN/$i
-    cp $CUPCAKE/{*$i.collapsed.group.txt*,*$i.collapsed.filtered.gff*,*$i.collapsed.filtered.abundance.txt*,*$i.collapsed.filtered.rep.fq*} .
+    cp $TOFU/{*$i.collapsed.group.txt*,*$i.collapsed.filtered.gff*,*$i.collapsed.filtered.abundance.txt*,*$i.collapsed.filtered.rep.fq*} .
     echo "Copied over files"
     ls 
     rename $i Sample *
@@ -52,8 +59,14 @@ EOL
 
 module load Miniconda2
 source activate sqanti2_py3
-
 echo "Processing samples for chain_samples.py"
-python $CHAIN_SCRIPT/chain_samples.py $CHAIN/Whole.config_file count_fl 2> Whole.chain_samples.log
-
+chain_samples.py $CHAIN/Whole.config_file count_fl 2> Whole.chain_samples.log
 source deactivate 
+
+###*************************************** Generate fusion.gtf using fusion script from Liz
+source $GENERAL_SCRIPT/Post_IsoSeq/Post_Isoseq3_Functions.sh
+# output in $TOFU_FUSION
+run_fusion_finder O18 K18 S18 L22 Q20 K24 Q21 K17 M21 O23 S23 K23
+
+###*************************************** SQANTI2 with fusion.gtf as input 
+run_sqanti2_QC_fusioninput O18 K18 S18 L22 Q20 K24 Q21 K17 M21 O23 S23 K23
