95b0a5c6eb5a7043359731898bc326eeddab8599
SziKayLeung <33493350+SziKayLeung@users.noreply.github.com>
2020-11-30 15:43:17 +0000

    SnakeFile First draft
    SnakeFile for IsoSeq Pipeline (first draft), sqanti_gtf_modify.py for IsoformSwitch

--- 3. Differential_Analysis/run_sqanti_gtf_modify.sh
+++ 3. Differential_Analysis/run_sqanti_gtf_modify.sh
@@ -11,7 +11,7 @@
 #SBATCH --output=run_sqanti_gtf_modify.o
 #SBATCH --error=run_sqanti_gtf_modify.e
 
-
+FUNCTIONS=/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/Scripts/IsoSeq_Tg4510/3_Differential_Analysis
 module load Miniconda2
 source activate sqanti2_py3
-python /gpfs/ts0/home/sl693/Scripts/sqanti_gtf_modify.py
+python $FUNCTIONS/sqanti_gtf_modify.py /gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Whole_Transcriptome/Individual_Samples/CHAIN/SQANTI3
