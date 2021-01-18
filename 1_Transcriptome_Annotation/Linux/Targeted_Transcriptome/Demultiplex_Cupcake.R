8316a35486a3b299415eb3496a3cf621a028f986
SziKayLeung <szikayleung@gmail.com>
2021-01-18 11:03:54 +0000

    Targeted Transcriptome Demultiplexing all batches
    Targeted Transcriptome Demultiplexing all batches

---
+++ 1_Transcriptome_Annotation/Linux/Targeted_Transcriptome/Demultiplex_Cupcake.R
@@ -0,0 +1,48 @@
+#!/usr/bin/env Rscript
+
+library("stringr")
+library("dplyr")
+library("tidyr")
+
+refine_dir = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/IsoSeq/REFINE"
+flnc_report <- list.files(path = refine_dir, pattern = "*flnc.report.csv", full.names = T)
+flnc_report_names <- list.files(path = refine_dir, pattern = "*flnc.report.csv")
+
+report <- lapply(flnc_report, function(x) read.csv(x))
+names(report) <- flnc_report_names
+
+# check if there are any overlapping ids across the samples
+all <- bind_rows(report, .id = "column_label")
+all$sample <- word(all$column_label,c(1), sep = fixed("."))
+n_occur <- data.frame(table(all$id))
+n_occur[n_occur$Freq > 1,]
+
+# check ids from flnc report is within the clustered.csv
+cluster_report <- read.csv("/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/All_Targeted_Merged/CLUSTER/All_Targeted_Merged.clustered.cluster_report.csv")
+
+# clusterd report contains a subset if id from flnc report
+# not included ids are lowly clustered
+setdiff(all$id, cluster_report$read_id)
+setdiff(cluster_report$read_id,all$id)
+
+# check ids from flnc report same as the read_stats.txt from All merged data
+read_stats <- read.table("/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/All_Targeted_Merged/TOFU/All_Targeted_Merged.collapsed.read_stat.txt",
+                         header = TRUE)
+
+# id from clustered report same as the read_stats
+setdiff(cluster_report$read_id, read_stats$id)
+setdiff(read_stats$id,cluster_report$read_id)
+
+# merged stats
+merged_stats <- merge(read_stats, all[,c("id","sample")], by = "id")
+
+# Unique Mapped Reads and FL
+unique <- merged_stats[merged_stats$stat == "unique",]
+final <- unique[,c("sample","pbid")]
+final_counts <- final %>% group_by(sample,pbid) %>% tally()
+final_output <- spread(final_counts, sample,n)
+colnames(final_output)[1] <- "id"
+final_output[is.na(final_output)] <- 0
+
+write.csv(final_output,"/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/All_Targeted_Merged/TOFU/All_Targeted_Merged.Demultipled_Abundance.txt",
+          row.names = FALSE, quote = FALSE)
