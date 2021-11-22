eb6eeafe79218c7427fc8baf594ab12ed1a8c9a9
sl693 <sl693@exeter.ac.uk>
2021-11-22 19:59:36 +0000

    Swan Scripts

--- 3_Differential_Analysis/Targeted_Transcriptome/Swan/targetedrun_swan_vis.py
+++ 3_Differential_Analysis/Targeted_Transcriptome/Swan/targetedrun_swan_vis.py
@@ -12,21 +12,72 @@ annot_gtf = '/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/reference_2019
 sg.add_annotation(annot_gtf)
 
 # set variables 
-targeted_gtf = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/Mouse/DiffAnalysis/COLLAPSE_FILTER/AllMouseTargeted_sqantisubset.final.classification.gtf"
-output_dir="/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/Vis"
+targeted_gtf = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/Mouse/DiffAnalysis/SQANTI3/AllMouseTargeted.collapsed_classification.filtered_lite.gtf"
+#targeted_gtf = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/Mouse/DiffAnalysis/SQANTI3/"
+output_dir = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/Vis"
+#abundance_file = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/Scripts/IsoSeq_Tg4510/3_Differential_Analysis/Targeted_Transcriptome/Swan/swan_count_simple.tsv"
+#novelinfo_dir="/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/Scripts/IsoSeq_Tg4510/3_Differential_Analysis/Targeted_Transcriptome/Swan/"
+#WT_2mos_gtf = targeted_gtf + "WT_2mos" + "_sqantitamafiltered.final.classification.gtf"
+#TG_2mos_gtf = targeted_gtf + "TG_2mos" + "_sqantitamafiltered.final.classification.gtf"
+#WT_8mos_gtf = targeted_gtf + "WT_8mos" + "_sqantitamafiltered.final.classification.gtf"
+#TG_8mos_gtf = targeted_gtf + "TG_8mos" + "_sqantitamafiltered.final.classification.gtf"
 
 # add datasets 
-sg.add_dataset('Targeted Mouse', targeted_gtf)
+sg.add_dataset('Targeted', targeted_gtf)
+#sg.add_dataset('WT 2 months', WT_2mos_gtf)
+#sg.add_abundance(abundance_file, count_cols='WT_2mos', dataset_name='WT 2 months', tid_col='transcript_id')
+
+#sg.add_dataset('WT 8 months', WT_8mos_gtf)
+#sg.add_abundance(abundance_file, count_cols='WT_8mos', dataset_name='WT 8 months', tid_col='transcript_id')
+
+#sg.add_dataset('TG 2 months', TG_2mos_gtf)
+#sg.add_abundance(abundance_file, count_cols='TG_2mos', dataset_name='TG 2 months', tid_col='transcript_id')
+
+#sg.add_dataset('TG 8 months', TG_8mos_gtf)
+#sg.add_abundance(abundance_file, count_cols='TG_8mos', dataset_name='TG 8 months', tid_col='transcript_id')
+
+
 #sg.save_graph('swan')
 #sg = swan.SwanGraph('swan.p')
 
-sg.plot_transcript_path('PB.3742.1', indicate_novel=True)
-swan.save_fig('PB.3742.1_Trem2.png')
-#sg.plot_graph('Trem2', indicate_novel=True)
-#swan.save_fig(output_dir+'/Trem2.png')
+# SNCA plots for Thesis to show redundant transcripts
+transcripts = ["PB.6948.1","PB.6948.115","PB.6948.15","PB.6948.160","PB.6948.35","PB.6948.81","PB.6948.7"]
+for t in transcripts: 
+  print("Generating figure:", t)
+  sg.plot_transcript_path(t, indicate_novel=True)
+  swan.save_fig(output_dir+'/SNCA/'+ t+'_Loop.png')
+  sg.plot_transcript_path(t, browser=True)
+  swan.save_fig(output_dir+'/SNCA/'+ t+'_Browser.png')
 
-#sg.plot_graph('Trem2')
 
-sg.plot_transcript_path('PB.5746.2', indicate_novel=True)
-swan.save_fig('PB.5746.2_Abca1.png')
+
+transcripts = ["PB.3915.175","PB.3915.120","PB.3915.5"]
+for t in transcripts: 
+  print("Generating figure:", t)
+  sg.plot_transcript_path(t, indicate_novel=True)
+  swan.save_fig(output_dir+ t+'_Loop.png')
+  sg.plot_transcript_path(t, browser=True)
+  swan.save_fig(output_dir+ t+'_Browser.png')
+  
+
+transcripts = ["PB.2634.299","PB.2634.365","PB.2634.2"]
+for t in transcripts: 
+  print("Generating figure:", t)
+  sg.plot_transcript_path(t, indicate_novel=True)
+  swan.save_fig(output_dir+'/'+ t+'_Loop.png')
+  sg.plot_transcript_path(t, browser=True)
+  swan.save_fig(output_dir+'/'+ t+'_Browser.png')
+  
+
+transcripts = ["PB.3915.1"]
+for t in transcripts: 
+  print("Generating figure:", t)
+  sg.plot_transcript_path(t, indicate_novel=True)
+  swan.save_fig(output_dir+'/'+ t+'_Loop.png')
+  sg.plot_transcript_path(t, browser=True)
+  swan.save_fig(output_dir+'/'+ t+'_Browser.png')
+
+
+
+
 
