0f196d82873c58acd935c4a68fd021df0d137cb2
sl693 <sl693@exeter.ac.uk>
2023-01-10 15:53:23 +0000

    Remove differential analysis Rscripts --> LOGEN

--- B_Targeted_Transcriptome/3_Differential_Analysis/Targeted_Transcriptome/tappas_variables.R
+++
@@ -1,8 +0,0 @@
-#### TAPPAS (Differential Analysis) ###################
-tappasiso_input_dir <- "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/Mouse/Post_IsoSeq/TAPPAS/Results/IsoSeq"
-tappasrna_input_dir <- "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/IsoSeq/Targeted_Transcriptome/Mouse/Post_IsoSeq/TAPPAS/Results/RNASeq"
-tappas_phenotype <- read.table("/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/Scripts/IsoSeq_Tg4510/Raw_Data/Targeted_Transcriptome/TargetedMouse_PhenotypeTAPPAS.txt", header = T) %>% mutate(col_names = paste0(group,".",sample))
-
-# Output from Tappas_DEA.R
-tappassig <- excel_sheets("/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/Scripts/IsoSeq_Tg4510/Figures_Thesis/Tables4Figures/DifferentialGeneExpression_Analysis.xlsx") %>%  set_names() %>%
-  purrr::map(read_excel, path = "/gpfs/mrc0/projects/Research_Project-MRC148213/sl693/Scripts/IsoSeq_Tg4510/Figures_Thesis/Tables4Figures/DifferentialGeneExpression_Analysis.xlsx")
