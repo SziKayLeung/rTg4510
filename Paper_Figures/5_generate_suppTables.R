8b8d6bce58bb36f40175161a6c9bfc67056251f9
sl693 <sl693@exeter.ac.uk>
2023-09-13 15:54:16 +0100

    Regenerated supplementary tables by genotype and pathology Simplified Table 1

--- Paper_Figures/5_generate_suppTables.R
+++ Paper_Figures/5_generate_suppTables.R
@@ -35,6 +35,7 @@ TSupp3 <- formulateStats(GlobalDESeq$resGeneAnno$wald,"gene")
 # DESeq2 transcript level expression analyses output for whole transcriptome data
 
 TSupp4 <- formulateStats(GlobalDESeq$resTranAnno$lrt)
+TSupp4B <- formulateStats(GlobalDESeq$resTranAnno$wald)
 
 
 ### ------------- Supplementary Table 5 -------------------
@@ -53,11 +54,45 @@ TSupp5 <- dplyr::bind_rows(
   formulateStats(TargetedDESeq$ontResTranAnno$wald8mosUnique) %>% dplyr::mutate(Dataset = "ONT", Model = "Wald8mos")) %>%
   dplyr::select(Dataset, Model, everything())
 
-write.table(merge(TargetedDESeq$ontResTranAnno$wald$res_Wald, class.files$targ_all[,c("isoform","associated_gene","associated_transcript")], 
-                  by = "isoform", all.x = T), paste0(dirnames$targ_output,"/TargetedDESeq_ontResTranAnno_resWald.txt"), quote = F, row.names = F, sep = "\t")
+TargetedMergedDESeq$waldGenotypeAgeInteraction <- merge(TargetedMergedDESeq$waldGenotypeAgeInteraction,class.files$targ_all[,c("isoform","subcategory")], by = "isoform", all.x=T)
+TargetedMergedDESeq$waldGenotype <- merge(TargetedMergedDESeq$waldGenotype,class.files$targ_all[,c("isoform","subcategory")], by = "isoform", all.x=T)
 
-write.table(merge(TargetedDESeq$ontResTranAnno$lrt$res_LRT, class.files$targ_all[,c("isoform","associated_gene","associated_transcript")], 
-                  by = "isoform", all.x = T), paste0(dirnames$targ_output,"/TargetedDESeq_ontResTranAnno_resLRT.txt"), quote = F, row.names = F, sep = "\t")
+write.table(TargetedMergedDESeq$waldGenotypeAgeInteraction, paste0(dirnames$targ_output,"/TargetedDESeq_ontResTranAnno_resWaldGenotypeAge.txt"), quote = F, row.names = F, sep = "\t")
+write.table(TargetedMergedDESeq$waldGenotype, paste0(dirnames$targ_output,"/TargetedDESeq_ontResTranAnno_resWaldGenotype.txt"), quote = F, row.names = F, sep = "\t")
+
+top10Genotype <- TargetedMergedDESeq$waldGenotype %>% arrange(padj_ont) %>% .[1:10,"isoform"]
+bottom10Genotype <- TargetedMergedDESeq$waldGenotype %>% filter(padj_ont < 0.05) %>% arrange(-padj_ont) %>% .[1:10,"isoform"]
+
+pdf(paste0(output_dir,"/GenotypeTop10Bottom10.pdf"), width = 10, height = 8)
+for(i in c(top10Genotype, bottom10Genotype)){
+  g = class.files$targ_all[class.files$targ_all$isoform == i,"associated_gene"]
+  print(plot_transexp_overtime(g,TargetedDESeq$ontResTranAnno$wald$norm_counts,show="specific",rank=2,isoSpecific=c(i),setorder=c("CONTROL","CASE")))
+}
+dev.off()
+
+nrow(TargetedMergedDESeqSig$waldGenotypeAge)
+nrow(TargetedMergedDESeqSig$waldGenotypeAgeInteraction) 
+nrow(TargetedMergedDESeqSig$waldGenotype)
+nrow(TargetedMergedDESeqSig$lrt)
+InteractionOnly = setdiff(TargetedMergedDESeqSig$waldGenotypeAgeInteraction$isoform,TargetedMergedDESeqSig$waldGenotype$isoform)
+GenotypeAlone = setdiff(TargetedMergedDESeqSig$waldGenotype$isoform,TargetedMergedDESeqSig$waldGenotypeAgeInteraction$isoform)
+
+pdf(paste0(output_dir,"/GenotypeOnly.pdf"), width = 10, height = 8)
+for(i in GenotypeAlone){
+  g = class.files$targ_all[class.files$targ_all$isoform == i,"associated_gene"]
+  print(plot_transexp_overtime(g,TargetedDESeq$ontResTranAnno$wald$norm_counts,show="specific",rank=2,isoSpecific=c(i),setorder=c("CONTROL","CASE")))
+}
+dev.off()
+
+ 
+
+pdf(paste0(output_dir,"/InteractionOnly.pdf"), width = 10, height = 8)
+for(i in InteractionOnly){
+  g = class.files$targ_all[class.files$targ_all$isoform == i,"associated_gene"]
+  print(plot_transexp_overtime(g,TargetedDESeq$ontResTranAnno$wald$norm_counts,show="specific",rank=2,isoSpecific=c(i),setorder=c("CONTROL","CASE")))
+}
+dev.off()
+
 
 ### ------------- Supplementary Table 6 -------------------
 # DIU analyses output for targeted transcriptome data
--- Paper_Figures/6_generate_mainTable.R
+++ Paper_Figures/6_generate_mainTable.R
@@ -8,4 +8,11 @@ Table1 <- data.table::transpose(mTable1)
 colnames(Table1) <- c("All","Known","Novel","Coding","Canonical","Non canonical","A5A3","ES","IR","A5A3T","EST","IRT")
 rownames(Table1) <- colnames(mTable1)
 Table1$NonCoding <-(Table1$All-Table1$Coding)
-Table1 <- Table1 %>% select(All,Known,Novel,Coding,NonCoding,Canonical,`Non canonical`,A5A3,ES,IR,A5A3T,EST,IRT)
+Table1 <- Table1 %>% mutate(A5A3F = paste0(A5A3T," (",A5A3,")"), 
+                  ESF = paste0(EST," (",ES,")"),
+                  IRF = paste0(IRT," (",IR,")")) 
+Table1 <- Table1 %>% select(All,Known,Novel,Coding,NonCoding,A5A3F,ESF,IRF)
+
+
+### ---------- Table 2 --------------------
+class.files$targ_filtered[class.files$targ_filtered$isoform %in% TargetedDESeq$ontResTranAnno$wald$anno_res$isoform,c("isoform","structural_category","subcategory")]
